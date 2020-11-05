a7714eda1c261b0f494687f7c005ce5a8f71b7c9
Hope <hopegiometti@gmail.com>
2020-11-05 09:49:45 -0700

    getting there

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from 'react'
 
 export const Home = (props) => {
-    const [counter, setCounter] = React.useState(60)
+    const [counter, setCounter] = useState(60)
+    const [start, setStart] = useState(false)
 
-    useEffect(() => {
-        const timer =
-            counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-        return () => clearInterval(timer);
-    }, [counter])
+    // useEffect(() => {
+    //     const timer =
+    //         counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    //     return () => clearInterval(timer);
+    // }, [counter])
+
+    const startTimer = () => {
+        console.log("hi")
+        setStart(true)
+        timer()
+    }
+
+    const timer = () => {
+        if (counter > 0) {
+            setInterval(() => setCounter(counter-1), 1000)
+        } else {
+            // setCounter(60)
+            // setStart(false)
+            return () => clearInterval()
+        }
+    }
 
     return(<>
-        <h1>Time Remaining: {counter}</h1>
+        {start ? <><h1>Time Remaining: {counter}</h1></> : <h1>Click to Start Your Timer!</h1>}
+        <button onClick={startTimer}>Start Timer</button>
     </>)
 }
 
